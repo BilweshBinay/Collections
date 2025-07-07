17d460fe964cf1d262f864dac312b61cd5cb9e1c
Y P Bilwesh <157962500+BilweshBinay@users.noreply.github.com>
2025-07-07 21:31:30 +0530

    Update with NavigableSet

---
+++ java-collections/src/com/example/collection/navigableset/NavigableSetExample.java
@@ -0,0 +1,32 @@
+package com.example.collection.navigableset;
+
+
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+public class NavigableSetExample {
+    public static void main(String[] args) {
+        NavigableSet<Integer> set = new TreeSet<>();
+
+        set.add(10);
+        set.add(25);
+        set.add(35);
+        set.add(15);
+        set.add(36);
+        set.add(42);
+
+        System.out.println("Set : "+ set);
+        System.out.println("Lower set : "+set.lower(10));
+
+        System.out.println("Higher set : "+set.higher(42));
+
+        System.out.println("Floor set : "+set.floor(25));
+        System.out.println("Floor set : "+set.floor(36));
+
+        System.out.println("Ceiling set : "+set.ceiling(10));
+        System.out.println("Ceiling set : "+set.ceiling(36));
+
+        System.out.println("Poll First : "+set.pollFirst());
+        System.out.println("Set  :" +set);
+    }
+}
